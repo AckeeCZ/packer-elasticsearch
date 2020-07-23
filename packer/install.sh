c60beb4a007e938f5a635c8b87e650701a676cff
Tomáš Hejátko <tomas.hejatko@ackee.cz>
2020-07-23 14:41:03 +0200

    ⬆️ Bump ES version to 7.8.0

--- packer/install.sh
+++ packer/install.sh
@@ -2,14 +2,17 @@
 
 set -ue -o pipefail
 
-DEBIAN_FRONTEND=noninteractive sudo apt-get update
-DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends nmap htop pigz ncdu apt-transport-https
-DEBIAN_FRONTEND=noninteractive sudo apt-get -y dist-upgrade
+export DEBIAN_FRONTEND=noninteractive
 
-wget -q https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.5.2-amd64.deb
-wget -q https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.5.2-amd64.deb.sha512
-shasum -a 512 -c elasticsearch-7.5.2-amd64.deb.sha512
-sudo dpkg -i elasticsearch-7.5.2-amd64.deb
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends nmap htop pigz ncdu apt-transport-https
+sudo apt-get -y dist-upgrade
+ES_VERSION=7.8.0
+
+wget -q https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$ES_VERSION-amd64.deb
+wget -q https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$ES_VERSION-amd64.deb.sha512
+shasum -a 512 -c elasticsearch-$ES_VERSION-amd64.deb.sha512
+sudo dpkg -i elasticsearch-$ES_VERSION-amd64.deb
 
 sudo mkdir /etc/systemd/system/elasticsearch.service.d/
 echo "[Service]
