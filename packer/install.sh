498d19ccc2e9549855b8cd87b0a2ad4288dbddbe
Tomáš Hejátko <tomas.hejatko@ackee.cz>
2020-08-11 17:04:05 +0200

    ✨ Install new Stackdriver agent version
    It has ES monitoring setup removed

--- packer/install.sh
+++ packer/install.sh
@@ -4,20 +4,20 @@ set -ue -o pipefail
 
 export DEBIAN_FRONTEND=noninteractive
 
-sudo apt-get update
-sudo apt-get install -y --no-install-recommends nmap htop pigz ncdu apt-transport-https
-sudo apt-get -y dist-upgrade
+apt-get update
+apt-get install -y --no-install-recommends nmap htop pigz ncdu apt-transport-https
+apt-get -y dist-upgrade
 ES_VERSION=7.8.0
 
 wget -q https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$ES_VERSION-amd64.deb
 wget -q https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$ES_VERSION-amd64.deb.sha512
 shasum -a 512 -c elasticsearch-$ES_VERSION-amd64.deb.sha512
-sudo dpkg -i elasticsearch-$ES_VERSION-amd64.deb
+dpkg -i elasticsearch-$ES_VERSION-amd64.deb
 
-sudo mkdir /etc/systemd/system/elasticsearch.service.d/
+mkdir /etc/systemd/system/elasticsearch.service.d/
 echo "[Service]
-LimitMEMLOCK=infinity" | sudo tee /etc/systemd/system/elasticsearch.service.d/override.conf
+LimitMEMLOCK=infinity" > /etc/systemd/system/elasticsearch.service.d/override.conf
 
-sudo /usr/share/elasticsearch/bin/elasticsearch-plugin install -b discovery-gce
-sudo /usr/share/elasticsearch/bin/elasticsearch-plugin install -b repository-gcs
-sudo /usr/share/elasticsearch/bin/elasticsearch-plugin install -b analysis-icu
+/usr/share/elasticsearch/bin/elasticsearch-plugin install -b discovery-gce
+/usr/share/elasticsearch/bin/elasticsearch-plugin install -b repository-gcs
+/usr/share/elasticsearch/bin/elasticsearch-plugin install -b analysis-icu
--- packer/stackdriver.sh
+++ packer/stackdriver.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-echo "deb http://packages.cloud.google.com/apt google-cloud-monitoring-$(lsb_release -sc) main" | sudo tee --append /etc/apt/sources.list.d/google-cloud-monitoring.list
-echo "deb http://packages.cloud.google.com/apt google-cloud-logging-wheezy main" | sudo tee --append /etc/apt/sources.list.d/google-cloud-logging.list
-curl --connect-timeout 5 -s -f "https://packages.cloud.google.com/apt/doc/apt-key.gpg" | sudo apt-key add -
-sudo apt-get -qq update
-DEBIAN_FRONTEND=noninteractive sudo apt-get -y -q install stackdriver-agent google-fluentd google-fluentd-catch-all-config
-DEBIAN_FRONTEND=noninteractive sudo apt-get clean
-sudo rm -Rf /var/lib/apt/lists/*
+export DEBIAN_FRONTEND=noninteractive
 
-cd /opt/stackdriver/collectd/etc/collectd.d/
-sudo curl -O https://raw.githubusercontent.com/Stackdriver/stackdriver-agent-service-configs/master/etc/collectd.d/elasticsearch-1.conf
+echo "deb http://packages.cloud.google.com/apt google-cloud-monitoring-$(lsb_release -sc)-all main" > /etc/apt/sources.list.d/google-cloud-monitoring.list
+echo "deb http://packages.cloud.google.com/apt google-cloud-logging-$(lsb_release -sc)-all main" > /etc/apt/sources.list.d/google-cloud-logging.list
+curl --connect-timeout 5 -s -f "https://packages.cloud.google.com/apt/doc/apt-key.gpg" | apt-key add -
+apt-get -qq update
+apt-get -y -q install stackdriver-agent google-fluentd google-fluentd-catch-all-config
+apt-get clean
+rm -Rf /var/lib/apt/lists/*
